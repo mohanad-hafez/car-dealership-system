5b341b0481c85ff61b929b2c336862b0e19d7634
xAGS1 <163142251+xAGS1@users.noreply.github.com>
2024-03-16 03:55:58 +0300

    Search Budget
    - Search car using budget.

--- carDealership/src/carDealership/Main.java
+++ carDealership/src/carDealership/Main.java
@@ -3,286 +3,292 @@ package carDealership;
 import java.util.Scanner;
 
 public class Main {
-    public static Scanner input = new Scanner(System.in);
-    public static Dealership dealership = new Dealership("Saidawi Motors", "Riyadh", 20);
+	public static Scanner input = new Scanner(System.in);
+	public static Dealership dealership = new Dealership("Saidawi Motors", "Riyadh", 2);
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-        System.out.println("Welcome to the Dealership System!");
-        while (true) {
-        	 System.out.println("\nChoose an option:");
-             System.out.println("1. Display all vehicles");
-             System.out.println("2. Add a vehicle");
-             System.out.println("3. Sell a vehicle");
-             System.out.println("4. Remove a vehicle");
-             System.out.println("5. Edit a vehicle");
-             System.out.println("6. Sales history");
-             System.out.println("7. Search type");
-             System.out.println("8. Change text color");
-             System.out.println("9. Exit");
-             System.out.print("Enter your choice: ");
-             String choice = input.nextLine();
+		System.out.println("Welcome to the Dealership System!");
+		while (true) {
+			System.out.println("\nChoose an option:");
+			System.out.println("1. Display all vehicles");
+			System.out.println("2. Add a vehicle");
+			System.out.println("3. Sell a vehicle");
+			System.out.println("4. Remove a vehicle");
+			System.out.println("5. Edit a vehicle");
+			System.out.println("6. Sales history");
+			System.out.println("7. Search type");
+			System.out.println("8. Find car using budget");
+			System.out.println("9. Change text color");
+			System.out.println("10. Exit");
+			System.out.print("Enter your choice: ");
+			String choice = input.nextLine();
 
-            switch (choice) {
-                case "1":
-                    if (dealership.isEmpty()) {
-                        System.out.println("\nInventory is empty!");
-                        break;
-                    }
-                    dealership.displayAll();
-                    break;
-                case "2":
-                    if (dealership.isFull()) {
-                        System.out.println("Inventory is full!");
-                        break;
-                    }
-                    addVehicleMenu();
-                    break;
-                case "3":
-                    sellVehicleMenu();
-                    break;
-                case "4":
-                    removeVehicleMenu();
-                    break;
-                case "5":
-                    editVehicleMenu();
-                    break;
-                case "6":
-                    dealership.displaySalesHistory();
-                    break;
-                case "8":
-                	changeColorMenu();
-                	break;
-                case "7":
-                	searchVehicleMenu();
-         
-                	break;
-                case "9":
-                    System.out.println("Exiting the Dealership System. Goodbye!");
-                    return;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
-    }
+			switch (choice) {
+			case "1":
+				if (dealership.isEmpty()) {
+					System.out.println("\nInventory is empty!");
+					break;
+				}
+				dealership.displayAll();
+				break;
+			case "2":
+				if (dealership.isFull()) {
+					System.out.println("Inventory is full!");
+					break;
+				}
+				addVehicleMenu();
+				break;
+			case "3":
+				sellVehicleMenu();
+				break;
+			case "4":
+				removeVehicleMenu();
+				break;
+			case "5":
+				editVehicleMenu();
+				break;
+			case "6":
+				dealership.displaySalesHistory();
+				break;
+			case "9":
+				changeColorMenu();
+				break;
+			case "7":
+				searchCarMenu();
 
-    public static void addVehicleMenu() {
-        System.out.println("\n-------------------------------------------\n");
-        System.out.println("Add a Vehicle");
-        System.out.println("\nChoose an option:");
-        System.out.println("1. Car");
-        System.out.println("2. Motorcycle");
-        System.out.println("3. Exit");
+				break;
+			case "8":
+				budgetCarMenu();
 
-        String choice = input.nextLine();
+				break;
+			case "10":
+				System.out.println("Exiting the Dealership System. Goodbye!");
+				return;
+			default:
+				System.out.println("Invalid choice. Please try again.");
+			}
+		}
+	}
 
-        switch (choice) {
-            case "1":
-                addCarMenu();
-                break;
-            case "2":
-                addMotorcycleMenu();
-                break;
-            case "3":
-                return;
-            default:
-                System.out.println("Invalid choice. Please try again.");
-        }
-    }
+	public static void addVehicleMenu() {
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Add a Vehicle");
+		System.out.println("\nChoose an option:");
+		System.out.println("1. Car");
+		System.out.println("2. Motorcycle");
+		System.out.println("3. Exit");
 
-    public static void addCarMenu() {
-        System.out.println("\n-------------------------------------------\n");
-        System.out.println("Add a Car");
+		String choice = input.nextLine();
 
-        System.out.print("\nEnter the make: ");
-        String make = input.nextLine();
+		switch (choice) {
+		case "1":
+			addCarMenu();
+			break;
+		case "2":
+			addMotorcycleMenu();
+			break;
+		case "3":
+			return;
+		default:
+			System.out.println("Invalid choice. Please try again.");
+		}
+	}
 
-        System.out.print("Enter the model: ");
-        String model = input.nextLine();
+	public static void addCarMenu() {
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Add a Car");
 
-        System.out.print("Enter the color: ");
-        String color = input.nextLine();
+		System.out.print("\nEnter the make: ");
+		String make = input.nextLine();
 
-        System.out.print("Enter the year: ");
-        int year = input.nextInt();
+		System.out.print("Enter the model: ");
+		String model = input.nextLine();
 
-        System.out.print("Enter the price: ");
-        double price = input.nextDouble();
+		System.out.print("Enter the color: ");
+		String color = input.nextLine();
 
-        System.out.print("Enter the type: ");
-        input.nextLine();
-        String type = input.nextLine();
+		System.out.print("Enter the year: ");
+		int year = input.nextInt();
 
-        if (dealership.addVehicle(new Car(make, model, color, year, price, type))) {
-            System.out.println("Car added succesfully.");
-        } else {
-            System.out.println("Couldn't add car.");
-        }
-    }
+		System.out.print("Enter the price: ");
+		double price = input.nextDouble();
 
-    public static void addMotorcycleMenu() {
-        System.out.println("\n-------------------------------------------\n");
-        System.out.println("Add a Motorcycle");
+		System.out.print("Enter the type: ");
+		input.nextLine();
+		String type = input.nextLine();
 
-        System.out.print("\nEnter the make: ");
-        String make = input.nextLine();
+		if (dealership.addVehicle(new Car(make, model, color, year, price, type))) {
+			System.out.println("Car added succesfully.");
+		} else {
+			System.out.println("Couldn't add car.");
+		}
+	}
 
-        System.out.print("Enter the model: ");
-        String model = input.nextLine();
+	public static void addMotorcycleMenu() {
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Add a Motorcycle");
 
-        System.out.print("Enter the color: ");
-        String color = input.nextLine();
+		System.out.print("\nEnter the make: ");
+		String make = input.nextLine();
 
-        System.out.print("Enter the year: ");
-        int year = input.nextInt();
+		System.out.print("Enter the model: ");
+		String model = input.nextLine();
 
-        System.out.print("Enter the price: ");
-        double price = input.nextDouble();
-        input.nextLine();
+		System.out.print("Enter the color: ");
+		String color = input.nextLine();
 
-        System.out.print("Enter the handlebar type: ");
-        String handlebarType = input.nextLine();
+		System.out.print("Enter the year: ");
+		int year = input.nextInt();
 
-        if (dealership.addVehicle(new Motorcycle(make, model, color, year, price, handlebarType))) {
-            System.out.println("Motorcycle added successfully.");
-        } else {
-            System.out.println("Couldn't add Motorcycle.");
-        }
-    }
+		System.out.print("Enter the price: ");
+		double price = input.nextDouble();
+		input.nextLine();
 
-    public static void sellVehicleMenu() {
-        System.out.println("\n-------------------------------------------\n");
-        System.out.println("Sell a Vehicle");
+		System.out.print("Enter the handlebar type: ");
+		String handlebarType = input.nextLine();
 
-        System.out.print("\nEnter the id of the vehicle: ");
-        int id = input.nextInt();
-        input.nextLine();
+		if (dealership.addVehicle(new Motorcycle(make, model, color, year, price, handlebarType))) {
+			System.out.println("Motorcycle added successfully.");
+		} else {
+			System.out.println("Couldn't add Motorcycle.");
+		}
+	}
 
-        if (dealership.getIndexFromId(id) == -1) {
-            System.out.println("\nVehicle not found!");
-            return;
-        }
+	public static void sellVehicleMenu() {
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Sell a Vehicle");
 
-        System.out.print("Enter the buyer's name: ");
-        String buyerName = input.nextLine();
+		System.out.print("\nEnter the id of the vehicle: ");
+		int id = input.nextInt();
+		input.nextLine();
 
-        System.out.print("Enter the buyer's contact: ");
-        String buyerContact = input.nextLine();
+		if (dealership.getIndexFromId(id) == -1) {
+			System.out.println("\nVehicle not found!");
+			return;
+		}
 
-        Vehicle vehicle = dealership.getVehicleFromId(id);
+		System.out.print("Enter the buyer's name: ");
+		String buyerName = input.nextLine();
 
-        if (dealership.sellVehicle(vehicle, buyerName, buyerContact)) {
-            System.out.println("Vehicle sold successfully.");
-        } else {
-            System.out.println("Couldn't sell vehicle");
-        }
+		System.out.print("Enter the buyer's contact: ");
+		String buyerContact = input.nextLine();
 
-    }
+		Vehicle vehicle = dealership.getVehicleFromId(id);
 
-    public static void removeVehicleMenu() {
-        System.out.println("\n-------------------------------------------\n");
-        System.out.println("Remove a Vehicle");
+		if (dealership.sellVehicle(vehicle, buyerName, buyerContact)) {
+			System.out.println("Vehicle sold successfully.");
+		} else {
+			System.out.println("Couldn't sell vehicle");
+		}
 
-        System.out.print("\nEnter the id of the vehicle: ");
-        int id = input.nextInt();
-        input.nextLine();
+	}
 
-        if (dealership.getIndexFromId(id) == -1) {
-            System.out.println("\nVehicle not found!");
-            return;
-        }
+	public static void removeVehicleMenu() {
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Remove a Vehicle");
 
-        Vehicle vehicle = dealership.getVehicleFromId(id);
+		System.out.print("\nEnter the id of the vehicle: ");
+		int id = input.nextInt();
+		input.nextLine();
 
-        if (dealership.removeVehicle(vehicle)) {
-            System.out.println("Vehicle removed successfully.");
-        } else {
-            System.out.println("Couldn't remove vehicle");
-        }
-    }
+		if (dealership.getIndexFromId(id) == -1) {
+			System.out.println("\nVehicle not found!");
+			return;
+		}
 
-    public static void editVehicleMenu() {
-        System.out.println("\n-------------------------------------------\n");
-        System.out.println("Edit a Vehicle");
+		Vehicle vehicle = dealership.getVehicleFromId(id);
 
-        System.out.print("\nEnter the id of the vehicle: ");
-        int id = input.nextInt();
-        input.nextLine();
+		if (dealership.removeVehicle(vehicle)) {
+			System.out.println("Vehicle removed successfully.");
+		} else {
+			System.out.println("Couldn't remove vehicle");
+		}
+	}
 
-        if (dealership.getIndexFromId(id) == -1) {
-            System.out.println("\nVehicle not found!");
-            return;
-        }
-        System.out.println("\nEnter the new information");
-        Vehicle vehicle = dealership.getVehicleFromId(id);
-        vehicle.displayInfo();
+	public static void editVehicleMenu() {
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Edit a Vehicle");
 
-        if (vehicle instanceof Car) {
-            carEdit((Car) vehicle);
-        }
-        if (vehicle instanceof Motorcycle) {
-            motorcycleEdit((Motorcycle) vehicle);
-        }
-    }
+		System.out.print("\nEnter the id of the vehicle: ");
+		int id = input.nextInt();
+		input.nextLine();
 
-    public static void carEdit(Car c) {
-        System.out.print("\nEnter the make: ");
-        String make = input.nextLine();
+		if (dealership.getIndexFromId(id) == -1) {
+			System.out.println("\nVehicle not found!");
+			return;
+		}
+		System.out.println("\nEnter the new information");
+		Vehicle vehicle = dealership.getVehicleFromId(id);
+		vehicle.displayInfo();
 
-        System.out.print("Enter the model: ");
-        String model = input.nextLine();
+		if (vehicle instanceof Car) {
+			carEdit((Car) vehicle);
+		}
+		if (vehicle instanceof Motorcycle) {
+			motorcycleEdit((Motorcycle) vehicle);
+		}
+	}
 
-        System.out.print("Enter the color: ");
-        String color = input.nextLine();
+	public static void carEdit(Car c) {
+		System.out.print("\nEnter the make: ");
+		String make = input.nextLine();
 
-        System.out.print("Enter the year: ");
-        int year = input.nextInt();
+		System.out.print("Enter the model: ");
+		String model = input.nextLine();
 
-        System.out.print("Enter the price: ");
-        double price = input.nextDouble();
+		System.out.print("Enter the color: ");
+		String color = input.nextLine();
 
-        System.out.print("Enter the type: ");
-        input.nextLine();
-        String type = input.nextLine();
+		System.out.print("Enter the year: ");
+		int year = input.nextInt();
 
-        c.setMake(make);
-        c.setModel(model);
-        c.setColor(color);
-        c.setYear(year);
-        c.setPrice(price);
-        c.setType(type);
+		System.out.print("Enter the price: ");
+		double price = input.nextDouble();
 
-    }
+		System.out.print("Enter the type: ");
+		input.nextLine();
+		String type = input.nextLine();
 
-    public static void motorcycleEdit(Motorcycle m) {
-        System.out.print("\nEnter the make: ");
-        String make = input.nextLine();
+		c.setMake(make);
+		c.setModel(model);
+		c.setColor(color);
+		c.setYear(year);
+		c.setPrice(price);
+		c.setType(type);
 
-        System.out.print("Enter the model: ");
-        String model = input.nextLine();
+	}
 
-        System.out.print("Enter the color: ");
-        String color = input.nextLine();
+	public static void motorcycleEdit(Motorcycle m) {
+		System.out.print("\nEnter the make: ");
+		String make = input.nextLine();
 
-        System.out.print("Enter the year: ");
-        int year = input.nextInt();
+		System.out.print("Enter the model: ");
+		String model = input.nextLine();
 
-        System.out.print("Enter the price: ");
-        double price = input.nextDouble();
+		System.out.print("Enter the color: ");
+		String color = input.nextLine();
 
-        System.out.print("Enter the handlebar type: ");
-        input.nextLine();
-        String handlebarType = input.nextLine();
+		System.out.print("Enter the year: ");
+		int year = input.nextInt();
 
-        m.setMake(make);
-        m.setModel(model);
-        m.setColor(color);
-        m.setYear(year);
-        m.setPrice(price);
-        m.setHandlebarType(handlebarType);
+		System.out.print("Enter the price: ");
+		double price = input.nextDouble();
 
-    }
-   public static void searchVehicleMenu() {
+		System.out.print("Enter the handlebar type: ");
+		input.nextLine();
+		String handlebarType = input.nextLine();
+
+		m.setMake(make);
+		m.setModel(model);
+		m.setColor(color);
+		m.setYear(year);
+		m.setPrice(price);
+		m.setHandlebarType(handlebarType);
+
+	}
+
+	public static void searchCarMenu() {
 		if (!(dealership.isEmpty())) {
 
 			System.out.println("Enter type: ");
@@ -298,40 +304,64 @@ public class Main {
 						total++;
 					}
 				}
-			}System.out.printf("Total found: [%d]\n", total);
+			}
+			System.out.printf("Total found: [%d]\n", total);
 		} else
 			System.out.println("Sorry the inventory is empty.");
 	}
-    public static void changeColorMenu() {
-  	  System.out.println("\n-------------------------------------------\n");
-        System.out.println("Add a Vehicle");
-        System.out.println("\nChoose an option:");
-        System.out.println("1. Blue");
-        System.out.println("2. Green");
-        System.out.println("3. White");
-        System.out.println("4. Exit");
-  	 String choice = input.nextLine();
 
-       switch (choice) {
-           case "1":
-          	 System.out.println("\u001B[36m"); // Changes color to Blue.
-               break;
-           case "2":
-          	 System.out.println("\u001B[32m"); // Changes color to Green.
-          	
-               break;
-           case "3":
-          	 System.out.println("\u001B[0m"); // Changes color back to white.
-          	
-               break;
-           case "4":
-               return;
-           default:
-               System.out.println("Invalid choice. Please try again.");
-  }
-  
-  }
-    
-    
-    
+	public static void changeColorMenu() {
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Add a Vehicle");
+		System.out.println("\nChoose an option:");
+		System.out.println("1. Blue");
+		System.out.println("2. Green");
+		System.out.println("3. White");
+		System.out.println("4. Exit");
+		String choice = input.nextLine();
+
+		switch (choice) {
+		case "1":
+			System.out.println("\u001B[36m"); // Changes color to Blue.
+			break;
+		case "2":
+			System.out.println("\u001B[32m"); // Changes color to Green.
+
+			break;
+		case "3":
+			System.out.println("\u001B[0m"); // Changes color back to white.
+
+			break;
+		case "4":
+			return;
+		default:
+			System.out.println("Invalid choice. Please try again.");
+		}
+
+	}
+
+	public static void budgetCarMenu() {
+		if (!(dealership.isEmpty())) {
+
+			System.out.println("Enter budget: ");
+			String budget = input.nextLine();
+
+			for (int i = 0; i < budget.length(); i++) {
+				if (budget.charAt(i) < 48 || budget.charAt(i) > 57) {
+					System.out.println("Invaild Input, Please enter numbers/postive numbers.");
+					return;
+				}
+			}
+			{
+				int x = dealership.carBudget(Double.parseDouble(budget));
+				System.out.printf("Total [%d]\n", x );
+			}
+
+		}else
+			System.out.println("Sorry the inventory is empty.");
+	}
+	
+	// Main end.
+	
+
 }
