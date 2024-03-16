2a6768cd9afd6fdbc3c0d1638e7ca8edb88e9263
xAGS1 <163142251+xAGS1@users.noreply.github.com>
2024-03-16 03:50:44 +0300

    budget
    - Search car using budget
    
    
    
    
    *Worked*

---
+++ Dealership/src/carDealership/Car.java
@@ -0,0 +1,31 @@
+package carDealership;
+
+public class Car extends Vehicle{
+	String type;
+	
+	public Car(String make, String model, String color, int year, double price, String type) {
+		super(make, model, color, year, price);
+		this.type = type;
+	}
+	
+	public Car(Car c) {
+		this(c.make, c.model, c.color, c.year, c.price, c.type);
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void displayInfo() {
+		String carName = make + " " + model + " " + year;
+		System.out.println("ID: " + id);
+        System.out.println("Car: " + carName);
+        System.out.println("Color: " + color);
+        System.out.println("Type: " + type);
+        System.out.println("Price: " + price + " SAR");
+	}
+}
